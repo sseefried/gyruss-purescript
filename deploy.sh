a64a1394c57eae97bda8fee79053b8156f0b5939
Sean Seefried <sean.seefried@gmail.com>
2019-07-09 09:45:10 +1000

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-[ "$SEANSEEFRIED_DOT_ORG" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG SSH path to remote directory"; exit 1; } 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+[ "$SEANSEEFRIED_DOT_ORG_MACHINE" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG SSH path to remote directory"; exit 1; }
+[ "$SEANSEEFRIED_DOT_ORG_PATH" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG SSH path to remote directory"; exit 1; }
+
+SEANSEEFRIED_DOT_ORG="$SEANSEEFRIED_DOT_ORG_MACHINE:$SEANSEEFRIED_DOT_ORG_PATH"
+
+ssh "$SEANSEEFRIED_DOT_ORG_MACHINE" mkdir -p $SEANSEEFRIED_DOT_ORG_PATH/games/gyruss-ps
 
 rsync -avz html/* "$SEANSEEFRIED_DOT_ORG/games/gyruss-ps"
